a8217dbd0752942070eddf5e268d98645bc1e8fb
runninmonke <runninmonke@yahoo.com>
2015-11-19 15:45:22 -0600

    feat: Make details display reponsively

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,5 @@
 $('.projects').on('click','img', function(evt){
 	imgLg = evt.target.currentSrc.slice(0,-4) + "-lg" + evt.target.currentSrc.slice(-4);
 	$('.display img').attr({"src": imgLg, "class": "img-detail"});
-	$('.display').prepend('<p class="txt-detail">This is my first ever project. It is the website you are on right now, but if you look close you will notice somethings have changed since this screenshot was taken.</p>');
+	$('.display').prepend('<p class="txt-detail">This is my first ever project. It is the website you are on right now, but if you look close you will notice some things have changed since this screenshot was taken.</p>');
 });
