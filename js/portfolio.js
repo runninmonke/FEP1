89047760dc2ec5c0b68e9ec427292515de452051
runninmonke <runninmonke@yahoo.com>
2015-11-19 17:48:46 -0600

    refactor: Break display details functionality into seperate functions and adjust to remove bug on multiple clicks

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,24 @@
-$('.projects').on('click','img', function(evt){
+var messages = {
+	proj1: "This is my first ever project. I built it with some images I had laying around and it's the website you are on right now.",
+	proj2: "The next project is to build a resume website.",
+	proj3: "Eventually I'm going to build a game similar to the arcade classic, Frogger."
+}
+
+var projectClick = function(evt) {
+	//TODO: Add ability to toggle back to the main image
+	changeImg(evt);
+	displayDescription(evt);
+};
+
+var changeImg = function(evt) {
 	imgLg = evt.target.currentSrc.slice(0,-4) + "-lg" + evt.target.currentSrc.slice(-4);
 	$('.display img').attr({"src": imgLg, "class": "img-detail"});
-	$('.display').prepend('<p class="txt-detail">This is my first ever project. It is the website you are on right now, but if you look close you will notice some things have changed since this screenshot was taken.</p>');
-});
+};
+
+var displayDescription = function(evt) {
+	$('.display p').attr("class", "txt-detail");
+	$('.display p').html(messages[evt.target.parentElement.id]);
+};
+
+$('.projects').on('click','img', projectClick);
+	
