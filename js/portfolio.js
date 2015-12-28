fa9dd528828aa8db0e84f614efdb7ec4ffcac013
runninmonke <runninmonke@yahoo.com>
2015-12-27 21:37:07 -0600

    text: Try to fix lack of touch event response

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,17 +4,20 @@ $(function() {
 		proj1: {
 			message: "This is my first ever project. I built it with some images I had laying around and it's the website you are on right now. It's responsive to different display sizes",
 			srcset: 'imgs/proj-1-sm.jpg 600w, imgs/proj-1-lg.jpg 1140w',
-			class: 'img-detail'
+			class: 'img-detail',
+			src: "imgs/proj-1-lg.jpg"
 		},
 		proj2: {
 			message: 'An arcade game similar to Frogger. Built by implementing psuedo-classical object-oriented programming patterns. The image assets and game engine were provided by Udacity.',
 			srcset: '',
-			class: 'img-detail centered'
+			class: 'img-detail centered',
+			src: "imgs/proj-2-lg.jpg"
 		},
 		proj3: {
 		message: 'Neighborhood map application built with the Google Maps API. Utilizes other APIs for additional meteological and astronomical data.',
 		srcset: 'imgs/proj-3-sm.jpg 600w, imgs/proj-3-lg.jpg 1140w',
-		class: 'img-detail'
+		class: 'img-detail',
+		src: "imgs/proj-3-lg.jpg"
 		}
 	}
 
@@ -28,8 +31,7 @@ $(function() {
 	//Change image in main picture area to project's screenshot
 	var changeImg = function(evt) {
 		var projName = evt.target.parentElement.id;
-		var imgLg = evt.target.currentSrc.slice(0,-4) + '-lg' + evt.target.currentSrc.slice(-4);
-		$('.display img').attr({'src': imgLg, 'class': projects[projName].class, 'srcset': projects[projName].srcset, 'sizes': '100vw', 'alt': 'Enlargement of project screenshot'});
+		$('.display img').attr({'src': projects[projName].src, 'class': projects[projName].class, 'srcset': projects[projName].srcset, 'sizes': '100vw', 'alt': 'Enlargement of project screenshot'});
 	};
 
 	//Display project discription
